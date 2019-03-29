cda41c11bbd49568d644adae54718d39f5bb6ac1
RiptideTheDragon <rustybulletstmm@gmail.com>
2019-03-29 13:47:22 -0600

    Adjusted loading screen to have timer of 5 seconds.

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ var ipc = electron.ipcMain;
 
 app.on('ready', function(){
 	var primary;
+	var loading;
 	primary = new window({show: false});
-	primary.loadURL('file://' + __dirname + '/info.html');
-	primary.show();
+	loading = new window({width: 400, height: 300, frame: false, transparent: true,});
+	//Set up loading window
+	loading.show();
+	loading.center();
+	loading.loadURL('file://' + __dirname + '/infoLoading.html');
+	//Set up window
+	primary.loadURL('file://' + __dirname + '/infoPrimary.html');
+
+	primary.once('ready-to-show', function(){
+		setTimeout(function() {
+				loading.close();
+				primary.maximize();
+			}, 5000); //Load for 5 seconds
+	})
 });
