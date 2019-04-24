7cb5c71929f63c98cc87431ea63bf4fdbfcfd82f
Pujan Tandukar <pujan@pujans-mbp.lan>
2019-04-24 01:30:10 -0600

    Added Dropdown

--- index.js
+++ index.js
@@ -23,13 +23,3 @@ app.on('ready', function() {
 		}, 1500) //Load for 1.5 seconds
 	})
 })
-
-function initialize() {
-	var options = { atmosphere: true, center: [ 0, 0 ], zoom: 0 }
-	var earth = new WE.map('earth_div', options)
-	WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
-		minZoom: 0,
-		maxZoom: 5,
-		attribution: 'NASA'
-	}).addTo(earth)
-}
