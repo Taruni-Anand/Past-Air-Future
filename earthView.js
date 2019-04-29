d08586a6dfe8053a6b2c0944b98164bf8e4b561a
Pujan Tandukar <pujan@engr2-29-84-dhcp.int.colorado.edu>
2019-04-29 11:05:35 -0600

    organizing files with different js files

---
+++ earthView.js
@@ -0,0 +1,21 @@
+earth_div = document.getElementById('earth_div')
+
+var earth
+function initialize() {
+	earth = new WE.map(earth_div)
+	// earth = new WE.map('earth_div')
+	earth.setView([ 46.8011, 8.2266 ], 1)
+	WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '© OpenStreetMap contributors'
+	}).addTo(earth)
+
+	// Start a simple rotation animation
+	var before = null
+	requestAnimationFrame(function animate(now) {
+		var c = earth.getPosition()
+		var elapsed = before ? now - before : 0
+		before = now
+		earth.setCenter([ c[0], c[1] + 0.1 * (elapsed / 30) ])
+		requestAnimationFrame(animate)
+	})
+}
---
+++ slider.js
@@ -0,0 +1,10 @@
+$(function() {
+	$('#slider').slider()
+})
+
+var slider = document.getElementById('myRange')
+var output = document.getElementById('demo')
+output.innerHTML = slider.value
+slider.oninput = function() {
+	output.innerHTML = this.value
+}
